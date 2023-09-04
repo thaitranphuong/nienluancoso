1e16981ea36a2e7d6599e1b9358b4a79f4796d90
Thai Tran <tranphuongthai000@gmail.com>
2023-09-04 17:48:26 +0700

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAddressBook, faAngry, faBarChart, faCircleXmark } from '@fortawesome/free-regular-svg-icons';
+import { BrowserRouter } from 'react-router-dom';
+
+function App() {
+    return (
+        <div className="App">
+            <FontAwesomeIcon icon={faBarChart} />
+            <h1>Test</h1>
+        </div>
+    );
+}
+
+export default App;
